a767610bfc66d13b67e0cab05d5e29d1b3524d0c
Koriche <57522500+Koriche@users.noreply.github.com>
2020-04-16 15:15:19 +0200

    Update debut.php

--- debut.php
+++ debut.php
@@ -7,20 +7,20 @@
             <div>
                 <label for="name">Nom :</label>
 	           </br>
-                <input type="text" id="name" name="user_name">
+                <input type="text" id="name" name="user_name" placeholder="Entrez votre nom">
             </div>
 	       <div>
                 <label for="prenom">Prenom :</label>
 	               </br>
-                <input type="text" id="prenom" name="user_prenom">
+                <input type="text" id="prenom" name="user_prenom" placeholder="Entrez votre prenom">
             </div>
             <div>
                 <label for="mail">Mail :</label>
 	               </br>
-                <input type="text" id="mail" name="user_mail">
+                <input type="text" id="mail" name="user_mail" placeholder="Entrez votre mail">
             </div>
             <div class="button">
-                <button type="submit">Envoyer les infos</button>
+                <button type="submit">Validez</button>
             </div>
         </form>
     </body>
